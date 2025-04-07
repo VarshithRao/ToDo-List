c93483c0b4d23ddd2db8b44ef73e05386bfa9265
Varshit Rao <139155159+VarshithRao@users.noreply.github.com>
2025-04-07 16:17:22 +0530

    changed script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
+// Function to add task
 function addTask(){
     if(inputBox.value === ''){
         alert("You must write something!");
-    }
-    else{
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
@@ -13,28 +14,35 @@ function addTask(){
         li.appendChild(span);
     }
     inputBox.value = "";
-    //calling save data
     saveData();
 }
 
-listContainer.addEventListener("click",function(e){
+// Event listener to handle 'Enter' key press
+inputBox.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
+
+// Event listener for checking and deleting tasks
+listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
+    } else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
     }
 }, false);
 
-//to store the data in the browser
+// Function to store data in the browser
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
-//to display saved data in the browser
+// Function to display saved data
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
+
 showTask();
